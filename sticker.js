96091d59318b66286a77acc0791381e6826f4378
worldmaker <worldmaker18349276@gmail.com>
2021-06-13 13:05:27 +0800

    Create sticker.js

---
+++ sticker.js
@@ -0,0 +1,32 @@
+let x = 100;
+let y = 100;
+let sticker = document.createElement("div");
+sticker.style = "position:fixed;z-index:1000;left:"+x+"px;top:"+y+"px;background:Cornsilk;padding:10px;";
+sticker.contentEditable = true;
+sticker.innerHTML = "type something...";
+document.body.appendChild(sticker);
+
+let isDragging = false;
+sticker.addEventListener("mousedown", e => {
+  if ( e.target === sticker )
+    isDragging = true;
+});
+sticker.addEventListener("blur", e => {
+  if ( Array.from(sticker.childNodes).every(n => n.nodeName.toLowerCase()=="br") )
+    document.body.removeChild(sticker);
+});
+window.addEventListener("mouseup", e => {
+  isDragging = false;
+});
+window.addEventListener("mousemove", e => {
+  if ( isDragging ) {
+    x += e.movementX;
+    y += e.movementY;
+    sticker.style.left = x + "px";
+    sticker.style.top = y + "px";
+  }
+});
+
+document.execCommand('enableObjectResizing');
+document.execCommand('enableAbsolutePositionEditor');
+document.execCommand('enableInlineTableEditing');
