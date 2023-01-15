6ef152dd20ffaea20c9803f548d57d70799e4e1f
worldmaker <worldmaker18349276@gmail.com>
2023-01-15 17:53:57 +0800

    draw.js: double click to exit

--- draw.js
+++ draw.js
@@ -1,44 +1,58 @@
-let down = false,
-  prevX = 0,
-  prevY = 0,
-  canvas = document.createElement("canvas"),
-  ctx = canvas.getContext("2d"),
-  picker = document.createElement("input");
+let down = false;
+let prevX = 0;
+let prevY = 0;
+const parent = document.body;
+const canvas = document.createElement("canvas");
+const context = canvas.getContext("2d");
+const picker = document.createElement("input");
 
-canvas.width = document.body.scrollWidth;
-canvas.height = document.body.scrollHeight;
-canvas.style = "position:absolute;top:0;left:0;background:transparent;z-index:1000;";
-document.body.appendChild(canvas);
+canvas.width = parent.scrollWidth;
+canvas.height = parent.scrollHeight;
+canvas.style = "position:absolute;top:0;left:0;background:transparent;z-index:2147483646;cursor:pointer;";
+parent.appendChild(canvas);
 
 picker.type = "color";
 picker.value = "#000000";
-picker.style = "position:fixed;right:5px;bottom:5px;z-index:1001;";
-picker.addEventListener("change", e => {ctx.strokeStyle=ctx.fillStyle=picker.value;});
-document.body.appendChild(picker);
+picker.style = "position:fixed;right:5px;bottom:5px;z-index:2147483646;";
+picker.addEventListener("change", e => {
+  context.strokeStyle = context.fillStyle = picker.value;
+});
+parent.appendChild(picker);
 
-ctx.fillStyle = "black";
-ctx.strokeStyle = "black";
-ctx.lineWidth = 2;
+context.fillStyle = "black";
+context.strokeStyle = "black";
+context.lineWidth = 2;
 canvas.addEventListener("mousedown", e => {
   down = true;
-  if (e.ctrlKey) ctx.clearRect(e.pageX - 10, e.pageY - 10, 20, 20);
-  else ctx.fillRect(e.pageX - 1, e.pageY - 1, 1, 1);
+  if (e.ctrlKey) {
+    context.clearRect(e.pageX - 10, e.pageY - 10, 20, 20);
+  } else {
+    context.fillRect(e.pageX - 1, e.pageY - 1, 1, 1);
+  }
   prevX = e.pageX;
   prevY = e.pageY;
 });
 canvas.addEventListener("mousemove", e => {
   if (down) {
-    if (e.ctrlKey) ctx.clearRect(e.pageX - 10, e.pageY - 10, 20, 20);
-    else {
-      ctx.beginPath();
-      ctx.moveTo(prevX, prevY);
-      ctx.lineTo(e.pageX, e.pageY);
-      ctx.stroke();
-      ctx.closePath();
+    if (e.ctrlKey) {
+      context.clearRect(e.pageX - 10, e.pageY - 10, 20, 20);
+    } else {
+      context.beginPath();
+      context.moveTo(prevX, prevY);
+      context.lineTo(e.pageX, e.pageY);
+      context.stroke();
+      context.closePath();
     }
     prevX = e.pageX;
     prevY = e.pageY;
   }
 });
-canvas.addEventListener("mouseup", e => down=false);
-window.addEventListener("keydown", e => e.key == "Escape" && document.body.removeChild(canvas) && document.body.removeChild(picker), {once: true});
+canvas.addEventListener("mouseup", e => {
+  down = false;
+});
+canvas.addEventListener("dblclick", e => {
+  parent.removeChild(canvas);
+  parent.removeChild(picker);
+}, {
+  once: true
+});
