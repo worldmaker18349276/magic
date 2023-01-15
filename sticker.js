c12c274b5bfb8a44bf5d6674353ee8579763ef37
worldmaker <worldmaker18349276@gmail.com>
2023-01-15 18:15:21 +0800

    refine sticker.js

--- sticker.js
+++ sticker.js
@@ -1,24 +1,26 @@
 let x = 100;
 let y = 100;
-let sticker = document.createElement("div");
-sticker.style = "position:fixed;z-index:1000;left:"+x+"px;top:"+y+"px;background:Cornsilk;padding:10px;";
-sticker.contentEditable = true;
-document.body.appendChild(sticker);
-
 let isDragging = false;
-sticker.addEventListener("mousedown", e => {
-  if ( e.target === sticker )
-    isDragging = true;
-});
+const parent = document.body;
+const sticker = document.createElement("div");
+sticker.style = `position:fixed;z-index:2147483646;left:${x}px;top:${y}px;background:Cornsilk;padding:10px;`;
+sticker.contentEditable = true;
+parent.appendChild(sticker);
+
 sticker.addEventListener("blur", e => {
-  if ( Array.from(sticker.childNodes).every(n => n.nodeName.toLowerCase()=="br") )
-    document.body.removeChild(sticker);
+  if (!sticker.innerText.replace(/\s*/, ""))
+    parent.removeChild(sticker);
+});
+
+sticker.addEventListener("mousedown", e => {
+  if (e.target === sticker)
+    isDragging = true;
 });
 window.addEventListener("mouseup", e => {
   isDragging = false;
 });
 window.addEventListener("mousemove", e => {
-  if ( isDragging ) {
+  if (isDragging) {
     x += e.movementX;
     y += e.movementY;
     sticker.style.left = x + "px";
